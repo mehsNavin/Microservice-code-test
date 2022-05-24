65cf1786a5cbab0326710b5c2d5c2108d9bca77a
mehsNavin <mehala.g.13@gmail.com>
2022-05-24 22:03:21 +0930

    Added Product Table

---
+++ Spring/src/main/java/com/shopping/model/Product.java
@@ -0,0 +1,45 @@
+package com.shopping.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Product")
+public class Product {
+
+	@Id @GeneratedValue
+	@Column(name = "ProductCode")
+	private String productCode;
+	
+	@Column(name = "Cost")
+	private String Cost;
+	
+	@Column(name = "Status")
+	private String status;
+
+	public String getProductCode() {
+		return productCode;
+	}
+
+	public void setProductCode(String productCode) {
+		this.productCode = productCode;
+	}
+
+	public String getCost() {
+		return Cost;
+	}
+
+	public void setCost(String cost) {
+		Cost = cost;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	
+
+}
